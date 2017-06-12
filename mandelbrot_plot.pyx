c06e05d34ceb8cecda726919eac2efc9ae9c13e3
bbarros50 <benbarros50@gmail.com>
2017-06-12 10:55:37 -0500

    Add files via upload

--- mandelbrot_plot.pyx
+++ mandelbrot_plot.pyx
@@ -1,34 +1,80 @@
 %cython
-def mandelbrot_plot(float x_center, float y_center, float zoom, int max_iteration = 100):
+def mandelbrot_plot(float x_center, float y_center, float image_width = 4, int max_iteration = 500, int pixel_count = 500):
+    ''' Function plots Mandelbrot set in the complex plane. Last updated 6:00 pm 6/9/17 '''
     from sage.plot.colors import Color
     from sage.repl.image import Image
 
-    cdef int size, color_value, row, col, iteration
-    cdef float new_x, new_y, x, y
+    cdef int color_value, row, col, iteration, color_num, i, j
+    cdef float new_x, new_y, x_coor, y_coor
+    cdef base_color, color_list
+
+    #max_iteration = 400 - 50*log(image_width)
 
     y_center *= -1 # reflect image about x-axis
-    size = 500 # size of image, this should be an optional parameter as well.
 
-    #M = Image("RGB", (size,size), 'black') # create image
-    M = Image("RGB", (size,size)) # create image
+    M = Image("RGB", (pixel_count,pixel_count), 'black') # create image
     pixel = M.pixels() # get pixels
-    for row in range(size):
-        x = x_center + 4*(1/zoom)*(row-size/2)/size
-        for col in range(size): # loop through pixels
-            #y = y_center + 4*(1/zoom)*float(col-size/2)/size # Do I need the float?
-            y = y_center + 4*(1/zoom)*(col-size/2)/size
+
+    base_color = [70, 40, 240]
+    color_num = 20
+    color_list = []
+    for i in range(color_num):
+        color_list.append(base_color)
+        for j in range(3):
+            color_list[i][j] += i*(255-color_list[i][j])/color_num
+        color_list[i] = tuple(color_list[i])
+
+    for row in range(pixel_count):
+        x_coor = x_center + image_width*(row-pixel_count/2)/pixel_count # image_width is width of cartesian coordinates
+        for col in range(pixel_count): # loop through pixels
+            y_coor = y_center + image_width*(col-pixel_count/2)/pixel_count
 
 
             new_x,new_y = (0.0, 0.0) # compute the orbit of 0 under the map f(z) = z^2 + c
             iteration = 0
 
             while (new_x**2 + new_y**2 <= 4.0 and iteration < max_iteration): # escape condition
-                new_x,new_y = new_x**2 - new_y**2 + x, 2*new_x*new_y + y
+                new_x,new_y = new_x**2 - new_y**2 + x_coor, 2*new_x*new_y + y_coor
                 iteration += 1
-            if iteration == max_iteration:
-                color_value = 0
-            else:
-                color_value = iteration*10 % 255
 
-            pixel[row,col] = (color_value*100, color_value*2, color_value) # TODO: ADD OPTIONAL COLOR OPTIONS
+            if iteration < max_iteration/color_num:  # starts with it < max/colNum
+                pixel[row,col] = color_list[0]
+            elif iteration < 2*max_iteration/color_num:
+                pixel[row,col] = color_list[1]
+            elif iteration < 3*max_iteration/color_num:
+                pixel[row,col] = color_list[2]
+            elif iteration < 4*max_iteration/color_num:
+                pixel[row,col] = color_list[3]
+            elif iteration < 5*max_iteration/color_num:
+                pixel[row,col] = color_list[4]
+            elif iteration < 6*max_iteration/color_num:
+                pixel[row,col] = color_list[5]
+            elif iteration < 7*max_iteration/color_num:
+                pixel[row,col] = color_list[6]
+            elif iteration < 8*max_iteration/color_num:
+                pixel[row,col] = color_list[7]
+            elif iteration < 9*max_iteration/color_num:
+                pixel[row,col] = color_list[8]
+            elif iteration < 10*max_iteration/color_num:
+                pixel[row,col] = color_list[9]
+            elif iteration < 11*max_iteration/color_num:
+                pixel[row,col] = color_list[10]
+            elif iteration < 12*max_iteration/color_num:
+                pixel[row,col] = color_list[11]
+            elif iteration < 13*max_iteration/color_num:
+                pixel[row,col] = color_list[12]
+            elif iteration < 14*max_iteration/color_num:
+                pixel[row,col] = color_list[13]
+            elif iteration < 15*max_iteration/color_num:
+                pixel[row,col] = color_list[14]
+            elif iteration < 16*max_iteration/color_num:
+                pixel[row,col] = color_list[15]
+            elif iteration < 17*max_iteration/color_num:
+                pixel[row,col] = color_list[16]
+            elif iteration < 18*max_iteration/color_num:
+                pixel[row,col] = color_list[17]
+            elif iteration < 19*max_iteration/color_num:
+                pixel[row,col] = color_list[18]
+            elif iteration < max_iteration:
+                pixel[row,col] = color_list[19]
     return M
